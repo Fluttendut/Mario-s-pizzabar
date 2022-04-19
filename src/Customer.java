a608172d802fdfd6bab7fca0c1abf6b9508fc7bd
Ella Frost <ded_19@hotmail.com>
2022-04-19 17:25:32 +0200

    Tiny update?

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,7 @@
 public class Customer
 {
+    private String name;
+    private int number;
+    //I'm thinking that double makes more sense as it can be sorted?
+    private double time;
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,3 +1,8 @@
 public class Pizza
 {
+    //Unsure if this should be a String array with ingredients or if we make it multiple stings
+    private String ingredient;
+    private String name;
+    private int price;
+
 }
