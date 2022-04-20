654e8f0c274d88c13c7fdcc0bb26fc032aad4ddc
BasmaJawad <basma.jawad02@gmail.com>
2022-04-20 16:11:04 +0200

    TTilføjede pizzaer i menu

--- src/Controller.java
+++ src/Controller.java
@@ -5,10 +5,15 @@ public class Controller
     public static void main(String[] args)
     {
 
-    ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
-    pizzas.add(new Pizza("Magaritta", 1, 69.99, new String[]{"Tomatoes", "Cheese", "Oregano"}));
+    ArrayList<Pizza> pizzas = new ArrayList<>();
+    Pizza pizza1 = new Pizza("Magaritta", 1, 69.99, new String[]{"Tomatoes", "Cheese", "Oregano"});
+    Pizza pizza2 = new Pizza("Vesuvio", 2,57,new String[]{"tomatsouce", "ost", "skinke", "oregano"});
+    pizzas.add(pizza1);
+    pizzas.add(pizza2);
+
 
     //ikke færdig
-    System.out.println(pizzas[0]);
+    System.out.println(pizza1);
+    System.out.println(pizzas);
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,17 @@
 public class Menu
 {
 
+    //                                  Navn/ Num/ Pris/ Ingredienser /
+    private Pizza pizza1 = new Pizza("Vesuvio", 1,57,new String[]{"tomatsouce", "ost", "skinke", "oregano"});
+    private Pizza pizza2 = new Pizza("Amerikaner", 2,53,new String[]{"tomatsouce", "ost", "oksefars", "oregano"});
+    private Pizza pizza3 = new Pizza("Cacciatore", 3,57,new String[]{"tomatsouce", "ost", "pepperoni", "oregano"});
+    private Pizza pizza4 = new Pizza("Carbona", 4,63,new String[]{"tomatsouce", "ost", "kødsauce", "spaghetti","cocktailpølser","oregano"});
+    private Pizza pizza5 = new Pizza("Dennis", 5,65,new String[]{"tomatsouce", "ost", "skinke","pepperoni", "cocktailpølser","oregano"});
+    private Pizza pizza6 = new Pizza("Bertil", 6,57,new String[]{"tomatsouce", "ost", "bacon", "oregano",});
+    private Pizza pizza7 = new Pizza("Silvia", 7,61,new String[]{"tomatsouce", "ost", "pepperoni", "rød peber", "løg", "oregano"});
+    private Pizza pizza8 = new Pizza("Victoria", 8,61,new String[]{"tomatsouce", "ost", "skinke", "ananas","champignon", "løg","oregano"});
+
+
+
+
 }
