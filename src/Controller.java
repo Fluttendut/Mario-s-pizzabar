4793bf22720f8d18984689e01f1bcca4af41b50b
Ella Frost <ded_19@hotmail.com>
2022-04-20 07:06:14 +0200

    Pizza object arraylist made + attributes

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class Controller
 {
     public static void main(String[] args)
     {
 
+    ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+    pizzas.add(new Pizza("Magaritta", 1, 69.99, new String[]{"Tomatoes", "Cheese", "Oregano"}));
+
+    //ikke færdig
+    System.out.println(pizzas[0]);
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,8 +1,19 @@
 public class Pizza
 {
     //Unsure if this should be a String array with ingredients or if we make it multiple stings
-    private String ingredient;
     private String name;
-    private int price;
+    private int number;
+    private double price;
+    private String[] ingredients;
+
+
+    public Pizza(String name, int number, double price, String[] ingredients)
+    {
+        this.name = name;
+        this.number = number;
+        this.price = price;
+        this.ingredients = ingredients;
+
+    }
 
 }
